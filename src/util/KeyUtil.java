730e34a8b97cbf36f57e376839813488456c45fa
荆国帅 <18334705765@163.com>
2018-09-07 17:02:15 +0800

    new file:   t

--- src/util/KeyUtil.java
+++ src/util/KeyUtil.java
@@ -2,6 +2,13 @@ package util;
 
 import java.util.Date;
 
+/**
+ * KeyUtil.java 工具类
+ * 
+ * @author 荆国帅
+ * @date 2018年9月7日
+ * @version 1.0
+ */
 public class KeyUtil {
 	/**
 	 * 根据标识和时间戳进行处理,返回String类型字符串
