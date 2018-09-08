7721c3c04a0f04aa2755b02f066e2f31d844383c
荆国帅 <18334705765@163.com>
2018-09-08 13:04:18 +0800

    增加测试案例

---
+++ src/main/java/first/UserService.java
@@ -0,0 +1,13 @@
+
+package first;
+
+public interface UserService {
+
+	public String sayHello(String id);
+
+	public String test(int a,int b);
+
+	public String test2();
+
+}
+
---
+++ src/main/java/first/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package first.impl;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import first.UserService;
+
+@Service("userService")
+@Component
+public class UserServiceImpl implements UserService{
+
+ 
+
+	public String sayHello(String  id) {
+
+		System.out.println("hello world----------------------------");
+
+		StringBuffer sb=new StringBuffer();;
+
+		for (int i = 0; i < 10; i++) {
+
+			sb=sb.append("hello world-->"+i+"===="+id+"\n");
+
+		}
+
+		return sb.toString();
+
+	}
+
+	public String test(int a,int b) {
+
+		return (a+b)+"";
+
+	}
+
+	public String test2() {
+
+		String str="hello dubbo";
+
+		return str;
+
+	}
+
+}
